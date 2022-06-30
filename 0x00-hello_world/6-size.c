fdc154f88f29a9e97ec4c4fd77c504436a5308d1
Tman0202 <tomasmandefro@gmail.com>
2022-06-30 14:04:55 -0700

    ithink last

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
  * main - print the size of various types
- * return: 0 if exited properly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwise
  */
+
 int main(void)
 {
 int a;
