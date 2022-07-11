88159a9b2b07df9ff7ef18ab281a970bac6b33d9
Tman0202 <tomasmandefro@gmail.com>
2022-07-11 05:14:52 -0700

    secondcom

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  *swap_int- swaping variable
- **a: first var
- **b: second var
+ *@a: first var
+ *@b: second var
  */
 void swap_int(int *a, int *b)
-
+{
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
+}
