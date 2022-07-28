95e9b4a5d2a7cf954c1d883c7c8b6de59af2aec1
Tman0202 <tomasmandefro@gmail.com>
2022-07-28 14:08:40 -0700

    tomcom

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: MAIN.h
- */
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -12,4 +9,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endi
+#endif
